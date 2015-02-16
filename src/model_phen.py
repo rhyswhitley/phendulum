e5ae3d482a400d133ec9feb138b9ad04e0f4ef2e
Rhys Whitley <rhys.whitley@gmail.com>
2015-02-17 10:58:16 +1100

    default optim set to gradient descent on spring fitting for now; genetic algorithm takes too long (+5 hrs)

--- src/model_phen.py
+++ src/model_phen.py
@@ -25,9 +25,9 @@ def main():
 
     # map data transformations to each dataset imported
     cor_data = [ dh.grass_correct_data(rd) for rd in raw_data ]
-    p0=[10,0.1,0.3,1,2]
+    p0=[20, 0.1, 0.3, 1, 2]
     bounds = [(0,1000),(0,1),(0,1),(-10,10),(-10,10)]
-    par_table = mo.optimize_all_sampling( mo.optimize_func, \
+    par_table = mo.optimize_all_sampling( mo.minimize_func, \
                     spring_motion, cor_data, p0, bounds, \
                     ylabel="NDVI_grass", xlabel="SWC10" )
 
