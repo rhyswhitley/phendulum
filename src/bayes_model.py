4f1eea2ef52ef40e80285f293cd73a5249dced41
rhyswhitley <rhys.whitley@gmail.com>
2015-03-09 17:16:25 +1100

    changed k priors to exp, which is sampled from parent uniform distributions

--- src/bayes_model.py
+++ src/bayes_model.py
@@ -25,8 +25,8 @@ def main():
     cor_data = [ dh.grass_correct_data(rd) for rd in raw_data ]
 
     # test with Sturt Plains
-    #mcmc_wrap(cor_data[5])
-    [ mcmc_wrap(d,d["Site"][0]) for d in cor_data ]
+    mcmc_wrap(cor_data[1],cor_data[1].Site[0])
+    #[ mcmc_wrap(d,d["Site"][0]) for d in cor_data ]
 
 def mcmc_wrap(data, site):
     print("MCMC Optim Model on ===> ", site)
@@ -37,11 +37,18 @@ def mcmc_wrap(data, site):
 
 def mcmc_optim(ys, xs, site):
 
-    #k_0 = pymc.Exponential('k_0', 0.01, value=0.3)
-    k_1 = pymc.Uniform('k_1', 1, 1e3, value=10)
-    k_2 = pymc.Uniform('k_2', 0, 1e2, value=0)
-    k_3 = pymc.Uniform('k_3', -1e2, 1e2, value=1)
-    k_4 = pymc.Uniform('k_4', -1e2, 1e2, value=2)
+#    k_1 = pymc.Uniform('k_1', 1, 1e3, value=100)
+#    k_2 = pymc.Uniform('k_2', 0, 1e2, value=10)
+#    k_3 = pymc.Uniform('k_3', 0, 1e2, value=0.001)
+#    k_4 = pymc.Uniform('k_4', 0, 1e2, value=5)
+    b_1 = pymc.Uniform('b_1', -1e3, 1e3, value=1)
+    b_2 = pymc.Uniform('b_2', -1e3, 1e3, value=1)
+    b_3 = pymc.Uniform('b_3', -1e3, 1e3, value=1)
+    b_4 = pymc.Uniform('b_4', -1e3, 1e3, value=1)
+    k_1 = pymc.Exponential('k_1', b_1, value=10)
+    k_2 = pymc.Exponential('k_2', b_2, value=0.1)
+    k_3 = pymc.Exponential('k_3', b_3, value=1)
+    k_4 = pymc.Exponential('k_4', b_4, value=2)
 
     @pymc.deterministic
     def springModel(ks_1=k_1, ks_2=k_2, ks_3=k_3, ks_4=k_4, xs=xs):
@@ -51,9 +58,11 @@ def mcmc_optim(ys, xs, site):
 
     likelihood = pymc.Normal('likelihood', mu=springModel, tau=1, value=ys, observed=True)
 
-    bayesModel = pymc.Model([likelihood, k_1, k_2, k_3, k_4])
-    springPost = pymc.MCMC(bayesModel, db='pickle', dbname='../outputs/spring_trace_'+site)
+    bayesModel = pymc.Model([likelihood, k_1, k_2, k_3, k_4, b_1, b_2, b_3, b_4])
+    springPost = pymc.MCMC(bayesModel, db='pickle', dbname='../outputs/spring_trace_exp_'+site)
     springPost.sample(5e4, 2e4, 1)
+
+    pymc.Matplot.plot(springPost)
 
     return None
 
